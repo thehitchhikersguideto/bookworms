1f1cede9cde1b3bd626e1bd04c1e741d1f217c9e
alejandrogl <alegalvezlo2@gmail.com>
2023-02-27 12:05:07 +0100

    adding react frontend

---
+++ frontend/workout-reco-app/src/components/Description.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+
+function DescriptionSection() {
+  return (
+    <section>
+      <p class="description">Welcome to our gym website! We offer a variety of exercises and workout plans to help you achieve your fitness goals.</p>
+    </section>
+  );
+}
+
+export default DescriptionSection;
