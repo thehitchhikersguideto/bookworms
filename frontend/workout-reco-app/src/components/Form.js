132578f90005ce6088332b2d3038b19ad308fb3d
fheshiki <70349016+paches00@users.noreply.github.com>
2023-03-05 18:54:20 +0100

    Updated frontend for book recommendations

--- frontend/workout-reco-app/src/components/Form.js
+++ frontend/workout-reco-app/src/components/Form.js
@@ -9,39 +9,39 @@ export default function FormSection() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    // TODO: implement logic to recommend exercises based on input
+    // TODO: implement logic to recommend books based on input
   }
 
   return (
     <section class = "FormS">
-      <h2>Recommend Exercises</h2>
+      <h2>Recommend Books</h2>
       <form class="req_btn" onSubmit={handleSubmit}>
         <label className='form_input' >
-          Age:
+          Book Title:
           <input className="input" type="number" value={age} onChange={(event) => setAge(event.target.value)} />
         </label>
         <br />
         <label className='form_input' >
-          Weight (in kg):
+          Author:
           <input className="input" type="number" value={weight} onChange={(event) => setWeight(event.target.value)} />
         </label>
         <br />
         <label className='form_input' >
-          Height (in cm):
+          Genres:
           <input className="input" type="number" value={height} onChange={(event) => setHeight(event.target.value)} />
         </label>
         <br />
         <label className='form_input' >
           Goal:
           <select className="input" value={goal} onChange={(event) => setGoal(event.target.value)}>
-            <option value="">Select</option>
-            <option value="lose">Lose Weight</option>
-            <option value="gain">Gain Muscle</option>
-            <option value="maintain">Maintain Weight</option>
+            <option value="">Recommend by Similar Books</option>
+            <option value="lose">Recommend by Similar Authors</option>
+            <option value="gain">Recommend by Genres</option>
+            <option value="maintain">Random Recommendation</option>
           </select>
         </label>
         <br />
-        <button class="request_ex" type="submit">Recommend Exercises</button>
+        <button class="request_ex" type="submit">Recommend Books</button>
       </form>
     </section>
   );
