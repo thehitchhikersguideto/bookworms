68262c7795f82f32ea8442caf4a6e5fb6da4770c
fheshiki <70349016+paches00@users.noreply.github.com>
2023-03-05 18:54:20 +0100

    Updated frontend for book recommendations

--- frontend/workout-reco-app/src/components/Title.js
+++ frontend/workout-reco-app/src/components/Title.js
@@ -4,8 +4,8 @@ import './styles.css';
 export default function TitleSection() {
   return (
     <section>
-      <h1 class="title">WORKOUT RECOMMENDER</h1>
-      <h2 class = "popup">Ready To Exercise?</h2>
+      <h1 class="title">BOOK RECOMMENDER</h1>
+      <h2 class = "popup">Ready To Read?</h2>
     </section>
   );
 }
