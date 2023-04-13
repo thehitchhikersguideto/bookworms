1a03c80aa41b9b231c512112f40e34e0185f5d04
ZaneReda <59884549+ZaneReda@users.noreply.github.com>
2023-04-13 20:05:01 +0200

    basic back end to function with frontend, the cold_start_reco.py file will have the code to return books based on the form input, while the recommender.py will have the actual recommendation engine

---
+++ backend/app.py
@@ -0,0 +1,45 @@
+# Import required libraries and modules
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+import recommender
+
+app = Flask(__name__)
+CORS(app)  # Enable CORS
+
+# Connect to the MongoDB instance
+client = MongoClient("mongodb://botly:BjzvLQxlBIxWDmfm@<host>:<port>/<dbname>")
+db = client['Goodreads']
+books_collection = db['BookList']
+
+# Endpoint for searching books in the database
+@app.route('/api/search', methods=['GET'])
+def search_books():
+    query = request.args.get('query')
+    results = books_collection.find({"$or": [
+        {"title": {"$regex": query, "$options": "i"}},
+        {"author": {"$regex": query, "$options": "i"}}
+    ]})
+
+    # Format the results as expected by the frontend
+    formatted_results = [
+        {
+            "id": str(book["_id"]),
+            "title": book["title"],
+            "authors": [book["author"]],
+            "image": book.get("image", None),
+        }
+        for book in results
+    ]
+
+    return jsonify(formatted_results)
+
+# Endpoint for getting book recommendations
+@app.route('/api/recommend', methods=['POST'])
+def recommend_books():
+    user_books = request.json['books']
+    recommendations = recommender.get_recommendations(user_books)
+    return jsonify(recommendations)
+
+if __name__ == '__main__':
+    app.run(debug=True)
